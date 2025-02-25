599624ae99bdb7ba69f9bd6d5724e35675693538
Alok <advancedtechnology62@gmail.com>
2025-02-25 10:30:58 +0530

    Using your component to view all of the list of entities - Practice assignment -1

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,14 @@
-// write the book component code here
+import React from 'react';
+
+const BookCard = ({ image, name, genre, author }) => {
+  return (
+    <div style={{ border: '1px solid #ddd', padding: '15px', borderRadius: '8px', width: '250px', textAlign: 'center' }}>
+      <img src={image} alt={name} style={{ width: '100%', height: '200px', objectFit: 'cover', borderRadius: '8px' }} />
+      <h3>{name}</h3>
+      <p><strong>Genre:</strong> {genre}</p>
+      <p><strong>Author:</strong> {author}</p>
+    </div>
+  );
+};
+
+export default BookCard;
